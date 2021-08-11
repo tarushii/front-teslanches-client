98647c53a9781e949567082df8638ae2a0b85518
Uthan Hercules <uthan.hercules12@gmail.com>
2021-08-11 15:36:23 -0300

    Configuração inicial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // eslint-disable-next-line comma-dangle
+  document.getElementById('root')
+);
