496ea23c67f4d1379426cb32b07b80d3d8427d2e
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-13 20:49:01 -0300

    routes para todas pgs + toastfy conteiner

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { ToastContainer } from 'react-toastify';
+import Routes from './routes';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
+    <ToastContainer
+      position="top-left"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </React.StrictMode>,
-  // eslint-disable-next-line comma-dangle
   document.getElementById('root')
 );
