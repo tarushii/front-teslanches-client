47bbd9e1120a5a3caa4abf8d1bc0886adbe2c95b
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-14 11:02:28 -0300

    inputPassword implementado em sign-up

--- src/pages/signUp/index.js
+++ src/pages/signUp/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-undef */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
@@ -5,7 +6,7 @@
 import './styles.css';
 import '../../styles/global.css';
 import { React, useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { toast } from 'react-toastify';
@@ -14,11 +15,15 @@ import { schemaCadastro } from '../../validation/schema';
 import AuthContext from '../../context/AuthContext';
 import imageCenter from '../../assets/img-center-register.svg';
 import imageLogo from '../../assets/logo-register.svg';
+import InputPassword from '../../components/inputPassword';
 
 export default function SingUp() {
   const [erro, setErro] = useState('');
   const [carregando, setCarregando] = useState(false);
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
   const { token } = useContext(AuthContext);
+  const history = useHistory();
   const toastOk = 'ok';
   const toastErro = 'erro';
   const {
@@ -31,6 +36,9 @@ export default function SingUp() {
     setCarregando(true);
     setErro('');
 
+    if (password !== passwordCheck) {
+      return toast.error('As senhas precisam ser iguais');
+    }
     try {
       const { dados, ok } = await postAutenticado('/cadastro', data, token);
       if (!ok) {
@@ -44,8 +52,7 @@ export default function SingUp() {
       toast.error(error.message, { toastId: toastErro });
       setErro(error.message);
     }
-    recarregarPag();
-    handleClose();
+    history.push('/');
     toast.success('Cadastro realizado com sucesso', { toastId: toastOk });
   }
 
@@ -61,28 +68,34 @@ export default function SingUp() {
       <img id="centroRegister" src={imageCenter} alt="imagem garota olhando celular" />
       <section className="telaRegistroFormulario">
         <main>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="mb1rem">Novo produto</h1>
+          <form autoComplete={false}>
+            <h1 className="mb1rem">Novo usuario</h1>
             <div className="flexColunm mb1rem ">
-              <label htmlFor="nomeRestaurante">Nome</label>
-              <input id="nomeRestaurante" type="text" {...register('nome', { required: true })} />
+              <label htmlFor="nome">Nome</label>
+              <input id="nome" type="text" {...register('nome', { required: true })} />
             </div>
             <div className="flexColunm mb1rem ">
-              <label htmlFor="nomeRestaurante">Nome</label>
-              <input id="nomeRestaurante" type="text" {...register('nome', { required: true })} />
+              <label htmlFor="email">Email</label>
+              <input id="email" type="email" {...register('email', { required: true })} />
             </div>
             <div className="flexColunm mb1rem ">
-              <label htmlFor="telefone">telefone</label>
-              <input id="telefone" type="number" {...register('telefone', { required: true })} />
+              <label htmlFor="telefone">Telefone</label>
+              <input id="telefone" type="number" placeholder="DDD 12345 1234" {...register('telefone', { required: true, minLength: 11 })} />
             </div>
-            <div className="flexColunm mb1rem ">
-              <label htmlFor="senha">Senha</label>
-              <input id="senha" type="password" {...register('senha', { required: true, minLength: 8 })} />
-            </div>
-            <div className="flexColunm mb1rem ">
-              <label htmlFor="senhaConfere">Repita a senha</label>
-              <input id="senhaConfere" type="text" {...register('senhaConfere', { required: true, minLength: 8 })} />
-            </div>
+            <InputPassword
+              id="senha"
+              label="Senha"
+              register={() => register('senha', { required: true, minLength: 8 })}
+              value={password}
+              setValue={setPassword}
+            />
+            <InputPassword
+              id="senhaConfere"
+              label="Repita a senha"
+              register={() => register('senhaConfere', { required: true, minLength: 8 })}
+              value={passwordCheck}
+              setValue={setPasswordCheck}
+            />
           </form>
         </main>
         <footer>
