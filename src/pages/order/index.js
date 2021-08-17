f5c3832d159179c6af51df24b716cd19e03e1431
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-17 18:09:35 -0300

    modal detalhes do produto estatico+styles

--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -2,16 +2,39 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import minusIcon from '../../assets/minusIcon.svg';
+import plusIcon from '../../assets/plusIcon.svg';
+import { postEstadoProduto, put } from '../../services/apiClient';
 import useStyles from './styles';
+import '../../styles/global.css';
 import './styles.css';
 
-export default function DetalheProduto() {
+import avatarRestaurante from '../../assets/avatar-padrao.gif'; // TODO - vir do back
+import fotoProduto from '../../assets/imageProduto.svg'; // TODO - vir do back
+import iconRelogio from '../../assets/relogio.svg';
+import iconMoney from '../../assets/money.svg';
+
+export default function DetalheProduto({ recarregarPag }) {
+  const [erro, setErro] = useState('');
+  const [quantidade, setQuantidade] = useState(0);
   const [open, setOpen] = useState(false);
+  const [carregando, setCarregando] = useState(false);
   const classes = useStyles();
   const customId = 'custom-id-yes';
+  const {
+    register, handleSubmit, formState: { errors }
+  } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: '',
+    defaultValues: {},
+    context: undefined,
+    criteriaMode: 'firstError',
+    shouldFocusError: true,
+    shouldUnregister: true,
+  });
 
   function handleClickOpen() {
     setOpen(true);
@@ -25,9 +48,46 @@ export default function DetalheProduto() {
     e.stopPropagation();
   }
 
+  async function onSubmit(data) {
+    setCarregando(true);
+    setErro('');
+
+    const { ...dadosAtualizados } = Object
+      .fromEntries(Object
+        .entries(data)
+        .filter(([, value]) => value));
+
+    try {
+      // const { dados, ok } = await put(`/produtos/${idProduto}`, dadosAtualizados, token);
+      // if (!ok) {
+      //   setErro(dados);
+      //   toast.error(dados);
+      //   return;
+      // }
+
+      // if (ativou) {
+      //   await postEstadoProduto(`/produtos/${idProduto}/ativar`, token);
+      //   toast.warn('O produto foi ativado!');
+      // } else {
+      //   await postEstadoProduto(`/produtos/${idProduto}/desativar`, token);
+      //   toast.warn('O produto foi desativado');
+      // }
+
+      setCarregando(false);
+    } catch (error) {
+      toast.error(error.message);
+      setErro(error.message);
+    }
+
+    handleClose();
+    recarregarPag();
+    toast.success('O pedido foi atualizado com sucesso!');
+  }
+
   return (
     <div onClick={(e) => stop(e)} className={classes.container}>
       <button
+        id="btDeAbrir"
         type="button"
         className="btLaranja mt2rem"
         onClick={handleClickOpen}
@@ -40,44 +100,50 @@ export default function DetalheProduto() {
         aria-labelledby="form-dialog-title"
       >
         <div className="flexColumn">
-          <div className="BodyDetalheProduto flexRow gap3rem px2rem">
-            <div className="fotoProduto posRelative">
-
-              <img src={fotoProduto} alt="foto do produto" className="fotoProduto" />
-
-              <img className="avatarRestaurante" src={avatarRestaurante} alt="avatar do restaurante" />
-
+          <div className="bodyDetalheProduto flexColumn gap3rem ">
+            <div className="conteinerFotoProduto posRelative mb3rem">
+              <img id="fotoProduto" src={fotoProduto} alt="foto do produto" className="fotoProduto" />
+              <img className="avatarDetalheRestaurante" src={avatarRestaurante} alt="avatar do restaurante" />
             </div>
-            <div>
+            <div className="conteinerdetalhesProduto px3rem">
               <h1>Nome do produto</h1>
-              <div className="flexRow">
-                <span>Pedido minimo do rest</span>
-                <span> tempo de entrega</span>
+              <div className="flexRow mt2rem contentBetween px2rem">
+                <span className="flexRow itemsCenter gap1rem">
+                  <img src={iconMoney} id="iconMoney" alt="icone dinheiro" />
+                  Pedido minimo: R$ 30,00
+                </span>
+                <span className="flexRow itemsCenter gap1rem">
+                  <img src={iconRelogio} id="iconRelogio" alt="icone relogio" />
+                  tempo de entrega: 1h - 1,5h
+                </span>
               </div>
               <form>
-                <div className="flexRow">
-                  <p>descricao</p>
-                  <span className="preco">$99</span>
+                <div className="flexRow mt3rem contentBetween px2rem mb3rem">
+                  <p>
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit ut aliquam,
+                    purus sit amet luctus venenatis
+                  </p>
+                  <span id="conteinerPreco">$99,99</span>
                 </div>
-                <div className="flexRow">
-                  click de -/+
-                  botao de submit
+                <div className="flexRow contentBetween itemsCenter px2rem">
+                  <div className="boxQuantidade flexRow contentBetween mt2rem">
+                    <button id="minus" type="button" onClick={() => setQuantidade(Math.max(quantidade - 1, 0))}><img id="minusIcon" src={minusIcon} alt="minus icon" /></button>
+                    <span id="textoQuantidade">{quantidade}</span>
+                    <button id="plus" type="button" onClick={() => setQuantidade(quantidade + 1)}><img id="plusIcon" src={plusIcon} alt="plus icon" /></button>
+                  </div>
+                  <button id="btAddCarrinho" className="btLaranja" type="submit" onClick={handleSubmit(onSubmit)}>
+                    Adicionar ao Carrinho
+                  </button>
                 </div>
-                <button type="submit">ir para cart</button>
+                <div className="flexRow contentCenter mt1rem">
+                  <button id="btTransparenteCinza" type="submit">Ir para a revisão do pedido</button>
+                </div>
               </form>
             </div>
           </div>
         </div>
-        <DialogActions className={classes.botoes}>
-          <button className="btTransparente" type="button" onClick={handleClose}>
-            Cancelar
-          </button>
-          <button className="btLaranja" type="submit" onClick={handleSubmit(onSubmit)}>
-            Salvar alterações
-          </button>
-        </DialogActions>
       </Dialog>
     </div>
-
   );
 }
