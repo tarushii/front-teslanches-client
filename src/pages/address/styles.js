190b0fc560ac19d30067e86b82ffe434bc1a30c9
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-19 18:52:26 -0300

    styles da tela add endereco

---
+++ src/pages/address/styles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+
+  botoes: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    gap: 24,
+    marginTop: 15,
+    marginRight: 24,
+    padding: 15,
+
+  },
+  MuiDialogPaper: {
+    borderRadius: 24,
+  }
+}));
