3395e9366d7db0ff7430c0a301cd58fc647c50a6
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-21 11:11:24 -0300

    criando um local storage para cart

--- src/hooks/useAuthProvider.js
+++ src/hooks/useAuthProvider.js
@@ -4,6 +4,7 @@ import { useLocalStorage } from 'react-use';
 export default function useAuthProvider() {
   const [tokenPersistido, setTokenPersistido, removeTokenPersistido] = useLocalStorage('TOKEN', null);
   const [usuario, setUsuario, removeUsuario] = useLocalStorage('USUARIO', {});
+  const [carrinhoLS, setCarrinhoLS, removeCarrinhoLS] = useLocalStorage('CARRINHO', []);
 
   const [user, setUser] = useState(usuario);
   const [token, setToken] = useState(tokenPersistido);
@@ -21,11 +22,21 @@ export default function useAuthProvider() {
     removeUsuario();
   };
 
+  function adicionarNoCarrinho(cart) {
+    setCarrinhoLS(cart);
+  }
+
+  function removerDoCarrinho() {
+    removeCarrinhoLS();
+  }
+
   return {
     user,
     token,
     logar,
     deslogar,
-
+    adicionarNoCarrinho,
+    removerDoCarrinho,
+    carrinhoLS
   };
 }
